30380d15e1778f06c4bd3c7adca2b8cde3852fc6
kunal dhanawade <kunaldhanawade279@gmail.com>
2024-09-24 12:00:35 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["aceternity.com", "images.unsplash.com", "assets.aceternity.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true, // Disable ESLint during build process
+  },
 };
 
 export default nextConfig;
